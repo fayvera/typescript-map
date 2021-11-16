a64cdefc601c1c3e3ef765d24ce0e0224770394c
Fay Vera <fayverawright@gmail.com>
2021-11-16 10:12:28 -0500

    specifies html element to be passed into customMap

---
+++ src/CustomMap.ts
@@ -0,0 +1,15 @@
+
+export class CustomMap {
+  // private so nobody can access this property outside of CustomMap
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    }) 
+  }
+}
